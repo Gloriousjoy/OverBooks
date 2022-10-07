a6011b3708ee3d4ea48a0c58d4ce730c75fdca92
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-07 15:44:09 +0300

    add readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter , Route, Routes} from "react-router-dom";
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import 
 import './index.css';
 
 
